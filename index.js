60ffefb58d29124c1f593b014a195d1dcd0ee01b
Ivan Zuev <113107003+silentdev92@users.noreply.github.com>
2022-09-24 21:03:22 +0800

    feat: Created router and added cors options

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ import './env.js'
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import routes from './routes/index.js'
 
 const PORT = process.env.PORT || 5000
+const CLIENT_URL = process.env.CLIENT_URL
+
+const corsOptions = { origin: CLIENT_URL, optionsSuccessStatus: 200 }
 
 const app = express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(helmet())
+
+app.use('/api', routes)
 
 app.listen(PORT, (err) => {
   if (err) return console.log(err)
---
+++ routes/index.js
@@ -0,0 +1,61 @@
+import { Router } from 'express'
+import axios from 'axios'
+const router = Router()
+
+const API_KEY = process.env.API_KEY
+
+router.get('/location', async (req, res) => {
+  try {
+    const { status, data } = await axios.get(
+      'https://api.openweathermap.org/geo/1.0/direct',
+      {
+        params: {
+          ...req.query,
+          appid: API_KEY,
+        },
+      }
+    )
+    res.status(status).send(data)
+  } catch (err) {
+    const { status, data } = err.response
+    res.status(status).send(data)
+  }
+})
+
+router.get('/current', async (req, res) => {
+  try {
+    const { status, data } = await axios.get(
+      'https://api.openweathermap.org/data/2.5/weather',
+      {
+        params: {
+          ...req.query,
+          appid: API_KEY,
+        },
+      }
+    )
+    res.status(status).send(data)
+  } catch (err) {
+    const { status, data } = err.response
+    res.status(status).send(data)
+  }
+})
+
+router.get('/forecast', async (req, res) => {
+  try {
+    const { status, data } = await axios.get(
+      'https://api.openweathermap.org/data/2.5/forecast',
+      {
+        params: {
+          ...req.query,
+          appid: API_KEY,
+        },
+      }
+    )
+    res.status(status).send(data)
+  } catch (err) {
+    const { status, data } = err.response
+    res.status(status).send(data)
+  }
+})
+
+export default router
